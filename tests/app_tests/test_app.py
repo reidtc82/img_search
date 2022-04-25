e2ca3ab2ec34e033a7a17bc1e22fdbf77dc45315
reidtc82 <reidtc82@gmail.com>
2022-04-24 23:02:00 -0700

    setting up flask fixture pytest

--- tests/app_tests/test_app.py
+++ tests/app_tests/test_app.py
@@ -1,7 +1,20 @@
+import os
+import tempfile
+
 import pytest
-from app import app, routes
+
+from app import app
 
 
-def test_index():
-    result = routes.index()
-    assert type(result) == str
+@pytest.fixture
+def client():
+    db_fd, app.app.config["DATABASE"] = tempfile.mkstemp()
+    app.app.config["TESTING"] = True
+
+    with app.app.test_client() as client:
+        with app.app.app_context():
+            app.init_db()
+        yield client
+
+    os.close(db_fd)
+    os.unlink(app.app.config["DATABASE"])
