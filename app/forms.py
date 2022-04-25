f806b42d6974f1ed0dde5ab4ec823801feca8439
reidtc82 <reidtc82@gmail.com>
2022-04-24 21:53:48 -0700

    image upload done

--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,15 @@
+from flask_uploads import UploadSet, IMAGES, configure_uploads
 from flask_wtf import FlaskForm
-from wtforms import SubmitField, FileField
-from wtforms.validators import DataRequired
+from flask_wtf.file import FileField, FileRequired, FileAllowed
+from wtforms import SubmitField
+from app import app
+
+images = UploadSet("images", IMAGES, default_dest=lambda x: "images")
+configure_uploads(app, images)
 
 
 class UploadForm(FlaskForm):
-    img_file = FileField("Image File", validators=[DataRequired()])
-
+    img_file = FileField(
+        "Image File", validators=[FileRequired(), FileAllowed(images, "Images only!")]
+    )
     submit = SubmitField("Search")
