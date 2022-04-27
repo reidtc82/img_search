72238bf030a58a41fcc3a95e340ffa45335e24d6
reidtc82 <reidtc82@gmail.com>
2022-04-26 17:46:44 -0700

    some test thingy

--- tests/app_tests/test_app.py
+++ tests/app_tests/test_app.py
@@ -8,13 +8,18 @@ from app import app
 
 @pytest.fixture
 def client():
-    db_fd, app.app.config["DATABASE"] = tempfile.mkstemp()
-    app.app.config["TESTING"] = True
+    # db_fd, app.app.config["DATABASE"] = tempfile.mkstemp()
+    app.config["TESTING"] = True
 
-    with app.app.test_client() as client:
-        with app.app.app_context():
-            app.init_db()
+    with app.test_client() as client:
+        # with app.app.app_context():
+        #     app.init_db()
         yield client
 
-    os.close(db_fd)
-    os.unlink(app.app.config["DATABASE"])
+    # os.close(db_fd)
+    # os.unlink(app.app.config["DATABASE"])
+
+
+def test_client(client):
+    rv = client.get("/")
+    assert rv
